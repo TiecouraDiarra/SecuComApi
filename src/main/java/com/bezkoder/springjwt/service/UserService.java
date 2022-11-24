b5e22b66b3490548f85aad3dde0cb741a803caf5
tddiarra <tiecouradiarra1999@gmail.com>
2022-11-24 15:03:50 +0000

    Après github integré Oauth

---
+++ src/main/java/com/bezkoder/springjwt/service/UserService.java
@@ -0,0 +1,16 @@
+package com.bezkoder.springjwt.service;
+
+import com.bezkoder.springjwt.models.ERole;
+import com.bezkoder.springjwt.models.Role;
+import com.bezkoder.springjwt.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface UserService {
+
+   // void addRoleToUser(String username, String roleName);
+    /*Role saveRole(Role role);
+    User saveUser(User user);*/
+}
